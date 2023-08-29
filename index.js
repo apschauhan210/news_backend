b6093b9f0a8260f4b6486a0f91729c8215828551
Anuj Pratap Singh <apschauhan181@gmail.com>
2023-08-29 15:46:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get("/fetch_news", async (req, res) => {
+    // console.log(req.body);
+    if(req.body.url) {
+        const response = await fetch(req.body.url);
+        const newsData = await response.json();
+        return res.json(newsData);
+    }
+    return res.json("URL required to fetch news");
+})
+
+app.get("/", (req, res) => {
+    res.json("Welcome to News API");
+})
+
+
+const port = 3030;
+app.listen(port, () => {
+    console.log("Server started");
+})
